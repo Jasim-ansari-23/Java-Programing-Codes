8826d4fbe9a37501daa6c44a9cbf691ad5860c3f
Jasim-ansari-23 <mrjasim87@gmail.com>
2024-02-04 17:58:34 +0530

    Added some operator codes

--- Local_codes/Ternary_Operator.java
+++ Local_codes/Ternary_Operator.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class Ternary_Operator {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int num;
