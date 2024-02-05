f166c55e6a2ba86bce86fa9f15186dad7180ffea
Jasim-ansari-23 <mrjasim87@gmail.com>
2024-02-05 14:37:21 +0530

    in

--- Main.java
+++ Main.java
@@ -4,10 +4,18 @@ public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int num;
-        System.out.println("Enter the number: ");
+        System.out.print("How many subjects do you have: ");
         num = sc.nextInt();
-        String type = (num % 2 == 0) ? "Even number" : "Odd number";
-        System.out.println(type);
+
+        int arr[] = new int[num];
+        for (int i = 0; i < num; i++) {
+            System.out.print("Enter the marks for subject " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("Marks of subject " + (i + 1) + ": " + arr[i]);
+        }
         sc.close();
     }
 }
