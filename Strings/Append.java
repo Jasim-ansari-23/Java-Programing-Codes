f8bd6ee662f94ec450e09385660eedb21f99e542
Jasim-ansari-23 <mrjasim87@gmail.com>
2024-05-01 09:56:32 +0530

    Everything just removed

--- Strings/Append.java
+++
@@ -1,9 +0,0 @@
-package Strings;
-
-public class Append {
-    public static void main(String[] args) {
-        StringBuilder nameP = new StringBuilder("Jasim ");
-        nameP.append("Ansari");
-        System.out.println(nameP);
-    }
-}
